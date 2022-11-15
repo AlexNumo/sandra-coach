f9cf5b4a91e73f8ab762563871a18207c1bf6f51
Arseniuk Oleksandr <korol1511@gmail.com>
2022-11-15 12:26:22 +0200

    Test 3

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,11 +1,16 @@
-import { Link } from "react-router-dom"
+import {
+  Wrapper,
+  LinkStyle
+} from './MainPage.styled';
 
 const MainPage = () => {
-  const schedule = '/schedule'
+  const Schedule = '/schedule';
+  const InfoCoachClients = '/info-coach-clients';
   return (
-    <>
-      <Link to={schedule}>schedule</Link>
-    </>
+    <Wrapper>
+      <LinkStyle to={Schedule}>Schedule</LinkStyle>
+      <LinkStyle to={InfoCoachClients}>Info of coach and clients</LinkStyle>
+    </Wrapper>
   )
 };
 
---
+++ src/Components/MainPage/MainPage.styled.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+`;
+const LinkStyle = styled(NavLink)`
+  font-family: 'Times New Roman', Times, serif;
+  font-size: 25px;
+  font-weight: 400;
+`;
+
+export {
+  Wrapper,
+  LinkStyle
+};
