9ba2fd3792d682528d29f7db5801c205f9aea82e
AlexNumo <84464243+AlexNumo@users.noreply.github.com>
2022-11-15 10:42:57 +0000

    Deploying to gh-pages from @ AlexNumo/sandra-coach@e61d888b01ea322a88a86a7388bcb46650ef5c6b 🚀

--- static/js/MainPage.de6e29fc.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunksandra=self.webpackChunksandra||[]).push([[475],{9885:function(e,n,r){r.r(n);var s=r(1087),u=r(184);n.default=function(){return(0,u.jsx)(u.Fragment,{children:(0,u.jsx)(s.rU,{to:"/schedule",children:"schedule"})})}}}]);
-//# sourceMappingURL=MainPage.de6e29fc.chunk.js.map
